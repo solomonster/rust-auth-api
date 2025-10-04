63d4e1feb810626f8f6865aad88f277f550c8006
solomonster <ismometrical@gmail.com>
2025-10-04 23:05:05 +0100

    Feat: Add server code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use axum::{Router, routing::get};
+mod routes;
+
+#[tokio::main]
+async fn main() {
+
+    let server_address: String = "127.0.0.1.8080".to_string();
+    let listener = TcpListener::bind(server_address)
+        .await
+        .expect("Unable to connect to the server");
+
+    let local_address = listener.local_addr().unwrap();
+
+    let app = routes::app().await;
+    axum::serve(listener, app)
+        .await
+        .expect("Error serving application");
+
+    println!("Listening on {}", local_address );
+
 }
