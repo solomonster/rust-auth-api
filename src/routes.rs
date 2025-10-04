e8f78d30493e004e25657c14e0383606030494fd
solomonster <ismometrical@gmail.com>
2025-10-04 23:07:00 +0100

    Feat: Create router file and add routes

---
+++ src/routes.rs
@@ -0,0 +1,16 @@
+use axum::{
+    middleware,
+    routing::{get, post},
+    Router,
+};
+use crate::{auth,services};
+
+pub async fn app()-> Router {
+    Router::new()
+    .route("/signin",post(auth::sign_in))
+    .route(
+        "/protected/",
+        get(services::hello).layer(middleware::from_fn(auth::authorize)),
+)
+        
+}
